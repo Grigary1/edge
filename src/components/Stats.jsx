6f3faed6fe181051e1258b0aee0d249f2b877c32
Grigary1 <grigaryvarkey16@gmail.com>
2025-02-10 15:44:26 +0530

    changes

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -57,7 +57,7 @@ const Stats = () => {
   return (
     <section
       ref={statsRef}
-      className="py-16 sm:py-20 bg-[hsl(0,0%,4%)] text-white text-center"
+      className="py-16 sm:py-20 bg-[#494447] text-white text-center"
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
